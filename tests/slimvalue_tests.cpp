adb171fac56993dfb76a9afd37fa6dae5130034c
Jeff Greer <greergan@gmail.com>
2026-04-22 18:57:00 -0400

    added tests

--- tests/slimvalue_tests.cpp
+++ tests/slimvalue_tests.cpp
@@ -2,9 +2,21 @@
 #include <slim/SlimValue.hpp>
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("SlimValue assingment from SlimValue in declaration") {
+TEST_CASE("SlimValue int assignment from SlimValue in declaration") {
 	slim::SlimValue a = 42;
 	REQUIRE(a == 42);
 	slim::SlimValue b = a;
 	REQUIRE(b == a);
+	REQUIRE(b == 42);
+}
+
+TEST_CASE("SlimValue string assignment from SlimValue in declaration") {
+	slim::SlimValue a = "my new world";
+	REQUIRE(a == "my new world");
+	slim::SlimValue b = a;
+	REQUIRE(b == a);
+	REQUIRE(b == "my new world");
+	a = "is awesome";
+	REQUIRE(a == "is awesome");
+	REQUIRE(b == "my new world");
 }
