51ddf7a367c965fe4082296a0981bd5ec76d2abc
Jeff Greer <greergan@gmail.com>
2026-04-22 17:37:59 -0400

    add better checking for SlimValue in constructor

---
+++ tests/slimvalue_tests.cpp
@@ -0,0 +1,9 @@
+#include <slim/SlimValue.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("SlimValue assingment from SlimValue in declaration") {
+	slim::SlimValue a = 42;
+	REQUIRE(a == 42);
+	slim::SlimValue b = a;
+	REQUIRE(b = a);
+}
