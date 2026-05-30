e071d5dc3c8b14a5b5c3a22075ba5e34fe7f9df6
Jeff Greer <greergan@gmail.com>
2026-05-29 19:08:35 -0700

    Added set_error overloads for SlimValue 	modified:   include/slim/SlimValue.hpp.in 	modified:   tests/slimvalue_tests.cpp

--- tests/slimvalue_tests.cpp
+++ tests/slimvalue_tests.cpp
@@ -20,3 +20,47 @@ TEST_CASE("SlimValue string assignment from SlimValue in declaration") {
 	REQUIRE(a == "is awesome");
 	REQUIRE(b == "my new world");
 }
+
+TEST_CASE("SlimValue set_error with code only") {
+	slim::SlimValue v = 42;
+	v.set_error(404);
+	REQUIRE(v.has_error());
+	REQUIRE(v.get_error().has_error_code());
+	REQUIRE(v.get_error().code() == 404);
+	REQUIRE(!v.get_error().has_error_message());
+}
+
+TEST_CASE("SlimValue set_error with message only") {
+	slim::SlimValue v = 42;
+	v.set_error("something went wrong");
+	REQUIRE(v.has_error());
+	REQUIRE(v.get_error().has_error_message());
+	REQUIRE(v.get_error().message() == "something went wrong");
+	REQUIRE(!v.get_error().has_error_code());
+}
+
+TEST_CASE("SlimValue set_error with code and message") {
+	slim::SlimValue v = 42;
+	v.set_error(500, "internal error");
+	REQUIRE(v.has_error());
+	REQUIRE(v.get_error().has_error_code());
+	REQUIRE(v.get_error().code() == 500);
+	REQUIRE(v.get_error().has_error_message());
+	REQUIRE(v.get_error().message() == "internal error");
+}
+
+TEST_CASE("SlimValue set_error does not clobber value") {
+	slim::SlimValue v = 99;
+	v.set_error(1, "oops");
+	REQUIRE(v.has_value());
+	REQUIRE(v == 99);
+	REQUIRE(v.has_error());
+}
+
+TEST_CASE("SlimValue set_error overwrite replaces previous error") {
+	slim::SlimValue v = 0;
+	v.set_error(1, "first");
+	v.set_error(2, "second");
+	REQUIRE(v.get_error().code() == 2);
+	REQUIRE(v.get_error().message() == "second");
+}
